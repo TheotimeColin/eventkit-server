fdc8ee1ffdb877195c1d8f59cc6af735a7fabf1e
Théotime COLIN <contact@theotimecolin.com>
2020-05-01 14:03:34 +0200

    feat(Articles): add dates

--- entities/article.js
+++ entities/article.js
@@ -10,7 +10,30 @@ const ArticleSchema = new mongoose.Schema({
     content: { type: String },
     cover: { type: String },
     thumbnail: { type: String },
+
+    published: { type: Boolean, default: false },
+    publishedDate: { type: Date },
+    modifiedDate: { type: Date },
+
+    hitsNumber: { type: Number, default: 0 },
     readTime: { type: Number, default: 5 }
+})
+
+ArticleSchema.pre('save', function(next) {
+    this.publishedDate = new Date()
+    this.modifiedDate = new Date()
+
+    next()
+})
+
+ArticleSchema.pre('findOneAndUpdate', function(next) {
+    const data = this.getUpdate()
+
+    data.publishedDate = data.publishedDate ? data.publishedDate : new Date()
+    data.modifiedDate = new Date()
+    this.update({}, data).exec()
+
+    next()
 })
 
 ArticleSchema.plugin(AutoIncrement, { inc_field: 'id', start_seq: 1000 })
