c5a2442299884218941960c56ebd9286dd42d2db
Théotime COLIN <contact@theotimecolin.com>
2020-05-04 18:50:12 +0200

    feat(Reactions): handle unique reactions

--- api/articles/reaction/get.js
+++ api/articles/reaction/get.js
@@ -4,9 +4,12 @@ const Reaction = require('../../../entities/reaction')
 module.exports = async function (req, res) {
     let errors = []
     let reaction = null
+    let unique = 0
+
+    if (req.query.unique) unique = 1
 
     try {
-        reaction = await Reaction.findByIdAndUpdate(req.query.id, { $inc: { count: 1 }})
+        reaction = await Reaction.findByIdAndUpdate(req.query.id, { $inc: { count: 1, uniqueCount: unique }})
 
         if (!reaction) throw new Error 
     } catch (err) {
