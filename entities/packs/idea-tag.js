5ceb5ed9662314309592a90b6095ca8ca51983fb
Théotime COLIN <contact@theotimecolin.com>
2020-05-27 08:09:01 +0200

    feat(IdeaTags): add emoji field

--- entities/packs/idea-tag.js
+++ entities/packs/idea-tag.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 
 const IdeaTag = new mongoose.Schema({
     label: { type: String },
+    emoji: { type: String },
     type: { type: String, default: 'tag' },
     kit: { type: mongoose.Schema.Types.ObjectId, ref: 'Kit' }
 })
